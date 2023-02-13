3a780953988bd151d7df9b27a037c403dff7a031
lwieckowski <lwieckowski@outlook.com>
2023-02-13 16:31:55 +0100

    Implement binary to decimal function

--- src/services/Conversion/index.js
+++ src/services/Conversion/index.js
@@ -1,7 +1,14 @@
 const binaryToDecimal = {
   from: "binary",
   to: "decimal",
-  conversionFunction: (value) => (value == "" ? "" : value * 2),
+  conversionFunction: value => {
+    const digits = value.length;
+    let result = 0;
+    for (let index = 0; index < digits; index++) {
+        result += value[index] * Math.pow(2, digits - index - 1);
+    }
+    return result;
+  }
 };
 
 export default binaryToDecimal;
